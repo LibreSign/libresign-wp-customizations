7d697a259a27d4f23339d3c9a3ffe7a0ac141639
YvesCesar <yvesamorim73@gmail.com>
2026-09-14 17:39:59 -0400

    fix: harden request input handling and declare the php requirement
    Signed-off-by: YvesCesar <yvesamorim73@gmail.com>

--- libresign-wp-customizations.php
+++ libresign-wp-customizations.php
@@ -15,6 +15,8 @@
  * Version:           0.0.1
  * Author:            LibreCode
  * Author URI:        https://github.com/LibreSign
+ * Requires at least: 6.7
+ * Requires PHP:      8.1
  * Text Domain:       libresign-wp-customizations
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
@@ -622,7 +624,7 @@ function libresign_is_root_my_account_endpoint_request() {
         return false;
     }
 
-    $request_uri  = isset( $_SERVER['REQUEST_URI'] ) ? sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) ) : '';
+    $request_uri  = isset( $_SERVER['REQUEST_URI'] ) ? sanitize_url( wp_unslash( $_SERVER['REQUEST_URI'] ) ) : '';
     $request_path = trim( (string) wp_parse_url( $request_uri, PHP_URL_PATH ), '/' );
 
     if ( '' === $request_path ) {
@@ -776,7 +778,7 @@ function libresign_get_subscription_for_status_change( $subscription_id, $new_st
  */
 function libresign_intercept_subscription_status_change() {
     if ( ! isset( $_GET['change_subscription_to'], $_GET['subscription_id'], $_GET['_wpnonce'] )
-        || ! function_exists( 'wc_clean' )
+        || ! function_exists( 'wcs_get_subscription' )
     ) {
         return;
     }
@@ -785,14 +787,14 @@ function libresign_intercept_subscription_status_change() {
         return;
     }
 
-    $new_status = wc_clean( wp_unslash( $_GET['change_subscription_to'] ) );
+    $new_status = sanitize_text_field( wp_unslash( $_GET['change_subscription_to'] ) );
 
     if ( ! libresign_get_subscription_confirmation_strings( $new_status ) ) {
         return;
     }
 
     $subscription = libresign_get_subscription_for_status_change( absint( wp_unslash( $_GET['subscription_id'] ) ), $new_status );
-    $nonce        = wc_clean( wp_unslash( $_GET['_wpnonce'] ) );
+    $nonce        = sanitize_text_field( wp_unslash( $_GET['_wpnonce'] ) );
 
     if ( ! $subscription
         || ! wp_verify_nonce( $nonce, $subscription->get_id() . $subscription->get_status() )
@@ -821,14 +823,14 @@ function libresign_render_subscription_change_confirmation() {
     if ( empty( $_GET['libresign_confirm_change'] )
         || empty( $_GET['libresign_confirm_subscription'] )
         || empty( $_GET['_wpnonce'] )
-        || ! function_exists( 'wc_clean' )
+        || ! function_exists( 'wcs_get_subscription' )
         || ! function_exists( 'is_account_page' )
         || ! is_account_page()
     ) {
         return;
     }
 
-    $new_status = wc_clean( wp_unslash( $_GET['libresign_confirm_change'] ) );
+    $new_status = sanitize_text_field( wp_unslash( $_GET['libresign_confirm_change'] ) );
     $strings    = libresign_get_subscription_confirmation_strings( $new_status );
 
     if ( ! $strings ) {
@@ -836,7 +838,7 @@ function libresign_render_subscription_change_confirmation() {
     }
 
     $subscription = libresign_get_subscription_for_status_change( absint( wp_unslash( $_GET['libresign_confirm_subscription'] ) ), $new_status );
-    $nonce        = wc_clean( wp_unslash( $_GET['_wpnonce'] ) );
+    $nonce        = sanitize_text_field( wp_unslash( $_GET['_wpnonce'] ) );
 
     if ( ! $subscription
         || ! wp_verify_nonce( $nonce, $subscription->get_id() . $subscription->get_status() )
